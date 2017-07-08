61d9d964993b14d54d4c6838bc1ad3ac0adaf580
StoicaMad <stoica.madalin01@gmail.com>
2017-07-08 23:07:58 +0300

    Add files via upload

---
+++ knight_rider_20_leduri.ino
@@ -0,0 +1,52 @@
+int pinArray[] = {A6,A6,A5,A4,A3,A2,A1,A0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,A6,A6};
+ 
+int pinCount = 24;
+int activePin = 1;
+int dx = 1;
+ int mintime = 0;
+ 
+ void setup() {
+ 
+  for (int i=0; i< pinCount; i++) {
+    if (pinArray[i] != -1) {
+      pinMode(pinArray[i], OUTPUT);
+    }
+  }
+}
+ 
+void loop() {
+ 
+ for (int i=0; i<pinCount; i++) {
+    digitalWriteNot(pinArray[i], LOW);
+  }
+ 
+  if (activePin == (pinCount-1)) {
+    activePin = (pinCount - 2);
+    dx = -1;
+  }
+ 
+ if (activePin == 0) {
+    activePin = 1;
+    dx = 1;
+  }
+ 
+   activePin += dx;
+ 
+
+ 
+  
+    digitalWriteNot(pinArray[activePin], HIGH);
+     digitalWriteNot(pinArray[activePin-dx], HIGH);
+    digitalWriteNot(pinArray[activePin-2*dx], HIGH);
+
+
+ 
+  delay(20);
+ 
+}
+ 
+void digitalWriteNot(int pin, boolean state) {
+  if (pin > -1) {
+    digitalWrite(pin, state);
+  }
+}
